9cc85332928fbae96d0249e81bf650e277cd5072
Stefan <stefan.c.holmes@gmail.com>
2017-10-12 20:58:20 +0100

    Handlebars working

--- app.js
+++ app.js
@@ -5,8 +5,25 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+// Handlebars
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+// Body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// Static folder
+app.use(express.static(`${__dirname}/public`));
+
+// Index
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
+    console.log(`${__dirname}\\public`);
 });
